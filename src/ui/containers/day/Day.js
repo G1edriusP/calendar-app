d8ca2bb244752d3976ec08a58a04d511862113ec
G1edriusP <giedrius.pakalniskis@gmail.com>
2021-02-05 11:17:58 +0200

    Added Day screen with formated date and holiday.

--- src/ui/containers/day/Day.js
+++ src/ui/containers/day/Day.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import {Text, View, Button} from 'react-native';
 
+// Utilities
+import {getMonthNameWithLimb} from '../../../common/Utilities';
+
 import Styles from './styles';
 
-const Day = ({navigation}) => (
-  <View styles={Styles.container}>
-    <Text>Day</Text>
-    <Button
-      title="Kalendorius"
-      onPress={() => navigation.navigate('Calendar')}
-    />
-  </View>
-);
+const Day = ({route, navigation}) => {
+  const {date, holiday, isHoliday} = route.params;
+
+  const monthIndex = date['month'];
+  const monthName = getMonthNameWithLimb(monthIndex - 1);
+
+  return (
+    <View styles={Styles.container}>
+      <Text>
+        {monthName} {date['day']}, {date['year']}
+      </Text>
+
+      {isHoliday ? <Text>{holiday['localName']}</Text> : null}
+
+      <Button title="Kalendorius" onPress={() => console.log(monthName)} />
+    </View>
+  );
+};
 
 export default Day;
